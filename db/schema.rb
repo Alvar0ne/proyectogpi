8f7635d2df4e75b521da8118eb593d269ed72124
Alvaro <elxalvaro@gmail.com>
2017-07-20 00:48:58 -0400

    postgress

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20170720000339) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "clientes", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "h1"
@@ -61,6 +70,17 @@ ActiveRecord::Schema.define(version: 20170720000339) do
     t.integer  "c4"
   end
 
+  create_table "comunas", force: :cascade do |t|
+    t.string   "nombre"
+    t.integer  "region_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "code"
+    t.string   "provincia"
+  end
+
+  add_index "comunas", ["region_id"], name: "index_comunas_on_region_id", using: :btree
+
   create_table "encuestados", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "h1"
@@ -106,6 +126,57 @@ ActiveRecord::Schema.define(version: 20170720000339) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
+  create_table "publication_attachments", force: :cascade do |t|
+    t.integer  "publication_id"
+    t.string   "avatar"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "publications", force: :cascade do |t|
+    t.string   "titulo"
+    t.integer  "precio"
+    t.string   "clasificacion"
+    t.string   "tipo"
+    t.integer  "superficie"
+    t.integer  "dormitorio"
+    t.integer  "baño"
+    t.integer  "estacionamiento"
+    t.text     "descripcion"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.integer  "user_id"
+    t.integer  "comuna_id"
+    t.integer  "region_id"
+    t.integer  "motivo"
+    t.string   "state",           default: "in_draft"
+    t.string   "nombre"
+    t.integer  "fono"
+  end
+
+  add_index "publications", ["comuna_id"], name: "index_publications_on_comuna_id", using: :btree
+  add_index "publications", ["region_id"], name: "index_publications_on_region_id", using: :btree
+  add_index "publications", ["user_id"], name: "index_publications_on_user_id", using: :btree
+
+  create_table "regions", force: :cascade do |t|
+    t.string   "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rols", force: :cascade do |t|
     t.string   "nombre"
     t.integer  "habilidad"
@@ -115,4 +186,29 @@ ActiveRecord::Schema.define(version: 20170720000339) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "nombre"
+    t.integer  "fono"
+    t.integer  "permission_level",       default: 1
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "comunas", "regions"
+  add_foreign_key "publications", "comunas"
+  add_foreign_key "publications", "regions"
+  add_foreign_key "publications", "users"
 end
